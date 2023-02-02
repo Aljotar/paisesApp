c2a448f00248e88dde0f16a0960f2aac48b04024
Alvaro Jotar <85590864+Aljotar@users.noreply.github.com>
2023-02-02 00:40:27 -0300

    se crea services para peticion http de paises y validaciones en pais html

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -10,10 +11,22 @@ export class PorPaisComponent {
 
   termino: string = ""
 
-  constructor() {}
+  hayError: boolean = false
+
+  constructor( private paisService: PaisService ) {}
 
   buscar() {
-    console.log(this.termino)
-  }
+  
+    this.hayError = false;
+    this.paisService.buscarPais( this.termino )
+      .subscribe( (paises) => {
+        console.log(paises)
 
+        
+
+
+      }, (err) => {
+        this.hayError = true
+      });
+  }
 }
