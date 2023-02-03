c5ffbf82e3bbf4251c5b57a5c29e6aeef5b782c2
Alvaro Jotar <85590864+Aljotar@users.noreply.github.com>
2023-02-03 02:28:26 -0300

    se finaliza busqueda por pais, se implementa debouncer

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -13,20 +14,30 @@ export class PorPaisComponent {
 
   hayError: boolean = false
 
+  paises: Country[] = [];
+
   constructor( private paisService: PaisService ) {}
 
-  buscar() {
+  buscar( termino: string ) {
   
     this.hayError = false;
-    this.paisService.buscarPais( this.termino )
+    this.termino = termino;
+
+    this.paisService.buscarPais( termino )
       .subscribe( (paises) => {
         console.log(paises)
-
-        
-
+        this.paises =  paises;
 
       }, (err) => {
-        this.hayError = true
+        this.hayError = true;
+        this.paises = [];
       });
+  }
+
+  sugerencias( termino: string ) {
+    this.hayError = false;
+
+    this.buscar( termino );
+
   }
 }
