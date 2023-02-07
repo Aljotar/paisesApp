60f5715bf0eae4affa68e2c356768143dc5a0ebc
Alvaro Jotar <85590864+Aljotar@users.noreply.github.com>
2023-02-07 00:28:52 -0300

    se añade busqueda por sugerencia

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,11 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+      li {
+        cursor: pointer
+      }
+    `
   ]
 })
 export class PorPaisComponent {
@@ -15,6 +20,10 @@ export class PorPaisComponent {
   hayError: boolean = false
 
   paises: Country[] = [];
+
+  paisesSugeridos: Country[] = [];
+
+  mostrarSugerencias: boolean = false;
 
   constructor( private paisService: PaisService ) {}
 
@@ -36,8 +45,20 @@ export class PorPaisComponent {
 
   sugerencias( termino: string ) {
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
 
-    this.buscar( termino );
+    this.paisService.buscarPais( termino )
+     .subscribe( pais => this.paisesSugeridos = pais.slice(0,5),
+     (err) => this.paisesSugeridos = []
+
+     );
+
+  }
+
+
+  buscarSugerido( termino: string ) {
+    this.buscar(  termino );
 
   }
 }
